29706c9af0221b483c1bea1db3663b206d58ad48
Mariastakh <32569878+Mariastakh@users.noreply.github.com>
2018-12-24 13:24:44 +0000

    Update PluginProcessor.cpp
    Phase reset and commenting

--- Simple_Warped_Delay/Source/PluginProcessor.cpp
+++ Simple_Warped_Delay/Source/PluginProcessor.cpp
@@ -29,14 +29,12 @@ Simple_warped_delayAudioProcessor::Simple_warped_delayAudioProcessor()
 	state->createAndAddParameter("PhasorFrequency", "PhasorFreq", "PhasorFreq", NormalisableRange<float>(1.0f, 10.0f, 0.001f), 1.0, nullptr, nullptr);
 	state->createAndAddParameter("PhasorDuration", "PhasorDur", "PhasorDur", NormalisableRange<float>(5000.0f, 22050.0f, 1.0f), 5000.0, nullptr, nullptr); // doesnt like th 5000.0f
 	state->createAndAddParameter("setSampleDuration", "Samples", "Samples", NormalisableRange<float>(1000.0f, 80000.0f, 1.0f), 1000.0, nullptr, nullptr);
-	//state->createAndAddParameter("PhasorSetStart", "phasorStart", "phasorStart", NormalisableRange<float>(0.0f, 22000.0f, 1.0f), 0.0, nullptr, nullptr);
-
+	state->createAndAddParameter("setSwitch", "SwitchSlider", "SwitchSlider", NormalisableRange<float>(1.0f, 2.0f, 1.0f), 1.0, nullptr, nullptr);
 
 	state->state = ValueTree("PhasorFrequency");
 	state->state = ValueTree("PhasorDuration");
 	state->state = ValueTree("setSampleDuration");
-	//state->state = ValueTree("PhasorSetStart");
-
+	state->state = ValueTree("setSwitch");
 
 	numSeconds = 5.0; // need to be able to vary this. also the max Duration of the phasor will be determined by the storage buffer size
 	numSamples = numSeconds * 44100;
@@ -169,29 +167,54 @@ void Simple_warped_delayAudioProcessor::processBlock (AudioBuffer<float>& buffer
 
 	for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
 		buffer.clear(i, 0, buffer.getNumSamples());
-
+    
+	// Grab values from the sliders:
 	float phasorFreq = *state->getRawParameterValue("PhasorFrequency");
 	float phasorDuration = *state->getRawParameterValue("PhasorDuration");
 	float desiredSamples = *state->getRawParameterValue("setSampleDuration");
-	//float phasorStart = *state->getRawParameterValue("PhasorSetStart");
+	//==========
+	
+	// This slider allows us to switch between the normal single-tap delay mode,
+	// and a version of the code where the delay line (oldBuffer) is read
+	// from at a range of indexes set by a phasor with changeable frequency
+	// and endPhase parameters. 
+	// This results in a delay which repeats the same segment in the delay buffer
+	// until the user changes the parameters. The frequency of 
+	// the phasor (the speed with which the index increments) influences the
+	// pitch of the playback. 
+	int garble = *state->getRawParameterValue("setSwitch");
 	
 	for (int sample = 0; sample < buffer.getNumSamples(); sample++)
 	{
-		int delayTime = osc1.phasor(phasorFreq, 0, phasorDuration);
-		//int delayTime = osc1.phasor(phasorFreq, phasorStart, phasorDuration);
-		//dry sig:
-		double originalLeft = buffer.getReadPointer(0)[sample];
-		double wetSig = buffer.getReadPointer(0)[sample];
+	    
+		// acquire the dry signal:
+		double dryLeft = buffer.getReadPointer(0)[sample];
+
 		// THE DELAY CODE:
-	   // delayTime = delayTime%desiredSamples;
+		double out;
+		if (garble == 1)
+		{
+		    // grab value from the storage buffer:
+			 out = oldBuffer[counter];
+			 resetPhase = true;
+			 
+		} else if(garble ==2){
+            // Reset phase if the slider has just been toggled;
+		    if(resetPhase == true) osc1.phaseReset(0);
+		    // set the index via the phasor value:
+		    int index = osc1.phasor(phasorFreq, 0, phasorDuration);
+		    // grab value from the storage buffer
+			out = oldBuffer[index]; //
+			resetPhase = false;
+		}
+		
+        // Set the current buffer index to the new dry signal mixed with
+        // a bit of the old: that's it!
+		oldBuffer[counter] = dryLeft + (out * 0.5);
 
-		double out = oldBuffer[delayTime]; //
-
-		oldBuffer[counter] = wetSig + (out * 0.5);
-		//Logger::outputDebugString("hello");
-		//Logger::outputDebugString(std::to_string(counter));
-		wetSig = out;
-
+		// This decides the delaytime, which is the amount of 
+		// samples between the current signal, and the delayed version
+		// stored in the buffer:
 		counter++;
 		if (counter >= desiredSamples) //numSamples
 		{
@@ -199,9 +222,10 @@ void Simple_warped_delayAudioProcessor::processBlock (AudioBuffer<float>& buffer
 		}
 
 		// OUTPUT:
-		*outputChannelData0 = wetSig;
-		*outputChannelData1 = wetSig;//buffer.getReadPointer(1)[sample];
-
+		*outputChannelData0 = out;
+		*outputChannelData1 = out;
+    
+		// Increment thru DAW buffer:
 		outputChannelData0++;
 		outputChannelData1++;
 
